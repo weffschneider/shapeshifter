38a60446e2f319c2c4f11241b7e8fd76701b9430
weffschneider <35546941+weffschneider@users.noreply.github.com>
2018-07-25 13:11:14 -0700

    added citation

--- trade_cobot.m
+++ trade_cobot.m
@@ -5,3 +5,5 @@ robot.power = [4 4 3.5 3 3.25 4.5 6 8 11 15 19.5]; % W
 
 robot.speed_max_endurance = 3;
 robot.speed_max_range = 8;
+
+% power vs speed from https://ieeexplore.ieee.org/stamp/stamp.jsp?tp=&arnumber=7943650&tag=1
--- trade_trex.m
+++ trade_trex.m
@@ -5,3 +5,5 @@ robot.power = [1800 1750 1680 1560 1440 1300 1250 1260 1300 1400 1500 1650 1800,
 
 robot.speed_max_endurance = 6;
 robot.speed_max_range = 9;
+
+% power vs. speed from https://ieeexplore.ieee.org/stamp/stamp.jsp?tp=&arnumber=7943650&tag=1
